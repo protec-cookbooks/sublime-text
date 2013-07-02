808e04da5337ae405b918b3ca02ed73cf10cf950
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-07-02 15:25:57 +0100

    Fix typo

--- recipes/free.rb
+++ recipes/free.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "/tmp/Sublime Text 2.0.1 x64.tar.bz2" do
+remote_file "/tmp/Sublime_Text_2.0.1_x64.tar.bz2" do
   source "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64.tar.bz2"
   mode 0644
   checksum "858df93325334b7c7ed75daac26c45107e0c7cd194d522b42a6ac69fae6de404"
@@ -23,11 +23,11 @@ end
 script "sublime-text-extract" do
   interpreter "bash"
   code <<-EOH
-    tar xfj /tmp/Sublime Text 2.0.1 x64.tar.bz2 -C /opt/sublime-text
+    tar xfj /tmp/Sublime_Text_2.0.1_x64.tar.bz2 -C /opt/sublime-text
   EOH
 end
 
 link "/usr/local/bin/sublime-text" do
-  to "/opt/sublime-text/Sublime Text 2/sublime-text"
+  to "/opt/sublime-text/Sublime Text 2/sublime_text"
   link_type :symbolic
 end
