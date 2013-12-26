4c28069042d0832f30dc58d09935a2c07f3a8737
Martin Meredith <martin@sourceguru.net>
2013-12-26 18:18:57 +0000

    update version

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,13 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "/tmp/sublime-text_build-3047_amd64.deb" do
-  source "http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3047_amd64.deb"
+remote_file "/tmp/sublime-text_build-3059_amd64.deb" do
+  source "http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3059_amd64.deb"
   mode 0644
-  checksum "e0af3d0c711988266b9a40bb7cb6b2198617080edde1bebdc66fc876a01ef470"
+  checksum "3dfad895479eb75fbf08f13ae6f0739b114c318b7a354dd1e632fa7453e5878b"
 end
 
 dpkg_package "sublime-text" do
-  source "/tmp/sublime-text_build-3047_amd64.deb"
+  source "/tmp/sublime-text_build-3059_amd64.deb"
   action :install
 end
